67099c233cda4b0690c2f0e2d5597d4c56b24459
belaidaymen444 <belaidaymen444@gmail.com>
2024-09-28 16:24:45 +0200

    feat(create server)

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 
-import fs from "fs"
-const data =  fs.readFileSync("./package.json")
-console.log(data.toString())
+import http from "http"
+const server = http.createServer((ruq, res)=>{
+    res.writeHead(200, {"Content-Type": "text/plain"});
+    res.end("Hello World\n");
+})
+server.listen(3000, "127.0.0.1", () => {
+    console.log("Hello wrold");
+    });
 
